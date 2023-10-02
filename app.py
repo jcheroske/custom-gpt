0e7cce1a797c8a632f646ded7e49221b53a92f54
Jay Cheroske <jay@portal.org>
2023-10-02 11:53:53 -0600

    Edits done on nas

--- app.py
+++ app.py
@@ -3,49 +3,62 @@ from langchain.chat_models import ChatOpenAI
 import gradio as gr
 import sys
 import os
+from watchdog.observers import Observer
+from watchdog.events import PatternMatchingEventHandler
+
+def initLogging():
+    sys.stdout = sys.stderr
+
+def initOpenAI(max_tokens):
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo", max_tokens=max_tokens)
+    set_global_service_context(ServiceContext.from_defaults(llm=llm))
+
+def readDocumentsFromDisk(docs_dir):
+    return SimpleDirectoryReader(input_dir=docs_dir, recursive=True).load_data()
+
+def indexDocuments(documents):
+    return GPTVectorStoreIndex.from_documents(documents, show_progress=True)
+
+def saveIndexToDisk(index, index_dir):
+    index.storage_context.persist(index_dir)
+
+def readIndexFromDisk(index_dir):
+    storage_context = StorageContext.from_defaults(persist_dir=index_dir)
+    return load_index_from_storage(storage_context)
+
+def indexDocumentsAndSave(index_dir):
+    documents = readDocumentsFromDisk()
+    index = indexDocuments(documents)
+    saveIndexToDisk(index, index_dir)
+    return index
+
+def createChatEngine(index):
+    return index.as_chat_engine(chat_mode="best")
+
+sys.stdout = sys.stderr
 
 print("Starting bootstrap")
 
-docs_directory = '/docs'
-index_dir = '/index'
+DOCS_DIR = '/docs'
+INDEX_DIR = '/index'
+MAX_TOKENS = 512
 
-max_input_size = 4096
-num_outputs = 512
-max_chunk_overlap = .5 # Unknown what this does
-chunk_size_limit = 600
+initLogging()
+initOpenAI(MAX_TOKENS)
 
-prompt_helper = PromptHelper(max_input_size, num_outputs, max_chunk_overlap, chunk_size_limit=chunk_size_limit)
-llm = ChatOpenAI(temperature=0.7, model_name="gpt-3.5-turbo", max_tokens=num_outputs)
-service_context = ServiceContext.from_defaults(llm=llm, prompt_helper=prompt_helper)
-set_global_service_context(service_context)
+numDocs = len(os.listdir(INDEX_DIR))
+index = readIndexFromDisk(INDEX_DIR) if numDocs > 0 else indexDocumentsAndSave(INDEX_DIR)
 
-index = None
-if len(os.listdir(index_dir)) > 0:
-    print('Loading index file from disk')
-    storage_context = StorageContext.from_defaults(persist_dir=index_dir)
-    index = load_index_from_storage(storage_context)
-
-else:
-    print('No index file found')
-
-    documents = SimpleDirectoryReader(docs_directory, recursive=True).load_data()
-    print(f"Loaded {len(documents)} docs")
-
-    index = GPTVectorStoreIndex.from_documents(documents)
-
-    index.storage_context.persist(index_dir)
-
-    print('Index file saved to disk')
-
+chat_engine = createChatEngine(index)
 
 def chatbot(input_text):
-    print(f"Chatbot called")
-    response = index.query(input_text, response_mode="compact")
-    return response.response
+    return chat_engine.chat(input_text).response
+
+# gr.ChatInterface(chatbot, title="Maia's Custom-trained AI Chatbot").launch(share=True)
 
 iface = gr.Interface(fn=chatbot,
                      inputs=gr.components.Textbox(lines=7, label="Enter your text"),
                      outputs="text",
-                     title="Custom-trained AI Chatbot")
+                     title="Maia's Custom-trained AI Chatbot")
 
 iface.launch(server_port=7860, share=True)
